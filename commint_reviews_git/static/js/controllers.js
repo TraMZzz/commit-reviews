4559e29df11e10060a7c3ad5516d549b0f949469
tramzzz <steh44@gmail.com>
2014-12-06 15:05:36 +0200

    show all information for projects and comments and commints

--- commint_reviews_git/static/js/controllers.js
+++ commint_reviews_git/static/js/controllers.js
@@ -1,11 +1,39 @@
 commentApp.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
-    this.submit = function(isValid, form_data) {
+    $scope.submit = function(isValid, form_data) {
         $scope.userprojects = ''
-        console.log(form_data)
         $http.get("https://api.github.com/users/"+ form_data.user +"/repos").
         success(function(data, status, headers, config) {
-            console.log(data)
             $scope.userprojects = data
+            console.log(data)
+      })
+    };
+    $scope.user = ''
+    $scope.usershow = function(user_data) {
+        $http.get(user_data).
+        success(function(data, status, headers, config) {
+            $scope.user = data
+      })
+    };
+    $scope.userproject = ''
+    $scope.commints = ''
+    $scope.projectshow = function(user_userproject) {
+        $http.get(user_userproject).
+        success(function(data, status, headers, config) {
+            $scope.userproject = data
+            console.log(data)
+            $http.get(user_userproject+'/commits').
+            success(function(data, status, headers, config) {
+                console.log(data)
+                $scope.commints = data
+            })
+      })
+    };
+    $scope.comments = ''
+    $scope.commentsshow = function(user_comments) {
+        $http.get(user_comments).
+        success(function(data, status, headers, config) {
+            $scope.comments = data
+            console.log(data)
       })
     };
 }]);

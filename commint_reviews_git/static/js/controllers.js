74233e65812c3ad9f7eb166a86c25e24b1cf104a
tramzzz <steh44@gmail.com>
2015-01-08 16:05:31 +0200

    controller refactor

--- commint_reviews_git/static/js/controllers.js
+++ commint_reviews_git/static/js/controllers.js
@@ -2,7 +2,13 @@
     "use strict"
     angular
         .module('commentApp')
-        .controller('FormController', ['Contact', function(Contact) {
+        .controller("FormController", FormControllerFunc)
+        .controller("UserController", UserControllerFunc)
+        .controller("ProjectController", ProjectControllerFunc)
+        .controller("CommintsController", CommintsControllerFunc);
+
+        FormControllerFunc.$inject = ['Contact'];
+        function FormControllerFunc(Contact) {
             var vm = this;
             vm.submit = function(isValid, form_data) {
                 if (isValid) {
@@ -11,24 +17,29 @@
                     });
                 }
             };
-        }])
-        .controller('UserController', ['ContactDetail', '$routeParams', function(ContactDetail, $routeParams) {
+        };
+
+        UserControllerFunc.$inject = ['ContactDetail', '$routeParams'];
+        function UserControllerFunc(ContactDetail, $routeParams) {
             var vmu = this;
             var user = $routeParams.userName;
             ContactDetail.submit(user).then(function(d) {
                 vmu.user = d;
             });
-        }])
-        .controller('ProjectController', ['Projects', '$routeParams', 'Contact', function(Projects, $routeParams, Contact) {
+        };
+
+        ProjectControllerFunc.$inject = ['Projects', '$routeParams', 'Contact'];
+        function  ProjectControllerFunc(Projects, $routeParams, Contact) {
             var vmp = this;
             var projectName = $routeParams.projectName;
             var user = Contact.getUser();
             Projects.submit(user, projectName).then(function(d) {
                 vmp.userproject = d;
             });
-        }])
-        .controller('CommintsController', ['ProjectsCommints', 'ProjectsComments', '$routeParams', 'Contact', 
-                                       function(ProjectsCommints, ProjectsComments, $routeParams, Contact) {
+        };
+
+        CommintsControllerFunc.$inject = ['ProjectsCommints', 'ProjectsComments', '$routeParams', 'Contact'];
+        function CommintsControllerFunc(ProjectsCommints, ProjectsComments, $routeParams, Contact) {
             var vmc = this;
             var project = $routeParams.projectName;
             var user = Contact.getUser();
@@ -41,5 +52,5 @@
                     vmc.comments = d;
                 });
             };
-        }])
+        };
 })();
